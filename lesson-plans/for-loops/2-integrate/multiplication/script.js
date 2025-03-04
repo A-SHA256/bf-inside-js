1495447da1f905ebf358ff5808c8af1a6838206d
A-SHA256 <arsanshataev0496@gmail.com>
2025-03-04 12:55:47 +0100

    add for loops exercises

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -1,11 +1,18 @@
 import { readNumber, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('do-math').addEventListener('click', () => {
   debugger;
 
   // read user values
-
+  const num1 = document.getElementById('left').value;
+  const num2 = document.getElementById('right').value;
+  console.log(num1, num2);
   // use a for loop to multiply the two numbers
-
+  let sum = 0;
+  for (let i = 1; i <= Number(num2); i++) {
+    sum += Number(num1);
+  }
+  
   // display the product
+  document.getElementById('product').innerText = sum;
 });
--- lesson-plans/for-loops/2-integrate/reverse-string/script.js
+++ lesson-plans/for-loops/2-integrate/reverse-string/script.js
@@ -4,8 +4,12 @@ document.getElementById(__).addEventListener('click', () => {
   debugger;
 
   // read user text
+  const userText = document.getElementById('user-text').value;
 
   // use a for loop that counts down (i--) to reverse the input
-
+  for (let i = userText.length - 1; i >= 0; i--) {
+    userText = userText[i];
+  }
   // display the reversed string
+  document.getElementById('reversed-output').innerText = userText;
 });
